3922699af216c8c9baa97a52e10fae56245c20ba
notakeyy <52760892+notakeyy@users.noreply.github.com>
2019-07-11 14:03:01 -0500

    Add files via upload

--- websitescript.js
+++ websitescript.js
@@ -8,3 +8,16 @@ function normalImg(x) {
   x.style.width = "200px";
   x.style.height = "200.px";
 }
+
+function showHideSection(x) {
+  var x = document.getElementById(x);
+  if (x.style.display == "none"){
+    x.style.display = "block";
+    document.getElementById("Hide Bio").innerHTML=("Hide Bio");
+  }
+  else{
+    x.style.display = "none";
+    document.getElementById("Show Bio").innerHTML=("Show Bio");
+  }
+
+}
